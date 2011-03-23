d91026197b2bfd30633c80f7a94db7a0f72aae25
Sebastien Estienne <sebastien.estienne@dailymotion.com>
2011-03-23 01:15:26 +0100

    improve keys

--- edgy.py
+++ edgy.py
@@ -1,33 +1,33 @@
 from redis import Redis
 from time import time
-# print r.get('toto')
 
 class Edgy(object):
 
-	def __init__(self, interval, count, prefix='edgy'):
+	def __init__(self, redis, interval, count, prefix='edgy'):
+		self.redis = redis
 		self.interval = interval
 		self.count = count
-		self.prefix = prefix
-		self.redis = Redis()
+		self.prefix = '%s-%s-%s' % (prefix, interval, count)
 
 	def update(self, key, amount=1):
-  		key = self.prefix + ':' + str(key) + ':' + str((int(time()) / self.interval + 1) * self.interval)
+  		key = self.prefix + ':' + str(key) + ':' + str(int(time()) / self.interval)
 		res = self.redis.incr(key, amount)
 		if res == amount:
 			self.redis.expire(key, self.interval * self.count)
 
   	def get(self, key):
-  		now = int(time()) / self.interval * self.interval
+  		now = int(time()) / self.interval
   		prefix = self.prefix + ':' + str(key) + ':'
   		res = 0
-  		for i in self.redis.mget([prefix + str(now + i * self.interval) for i in range(2 - self.count, 2)]):
+  		keys = [prefix + str(now + i) for i in range(1 - self.count, 1)]
+  		for i in self.redis.mget(keys):
   			if i:
   				res += int(i)
   		return res
 
 if __name__ == '__main__':
 	from time import sleep
-	e = Edgy(1, 5)
+	e = Edgy(Redis(), 1, 60)
 	while True:
 		e.update(45)
 		print e.get(45)

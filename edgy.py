191b8bd0d19074303886dba40cf73b0ebaf50b1a
Sebastien Estienne <sebastien.estienne@gmail.com>
2011-11-04 01:54:16 +0100

    add a layer for compatibility with CSD and add mget and mupdate

--- edgy.py
+++ edgy.py
@@ -10,20 +10,23 @@ class Edgy(object):
 
     def update(self, key, amount=1):
         now = int(time())
-        for name, (interval, count) in consolidations.items():
+        for name, (interval, count) in self.consolidations.items():
             prefix = '%s_%s/%s' % (self.prefix, name, key)
             if not interval:
                 self.redis.incr(prefix)
             else:
                 k = '%s/%s' % (prefix, now / interval)
-                res = self.redis.incr(k, amount)
-                if res == amount:
+                if self.redis.incr(k, amount) == amount:
                     self.redis.expire(k, interval * count)
+
+    def mupdate(self, keys):
+        for args in keys.items():
+            self.update(*args)
 
     def get(self, key):
         now = int(time())
         res = {}
-        for name, (interval, count) in consolidations.items():
+        for name, (interval, count) in self.consolidations.items():
             prefix = '%s_%s/%s' % (self.prefix, name, key)
             if not interval:
                 res[name] = self.redis.get(prefix)
@@ -33,39 +36,42 @@ class Edgy(object):
                 res[name] = sum([int(i) for i in self.redis.mget(keys) if i])
         return res
 
+    def mget(self, keys):
+        dict([(key, self.get(key)) for key in keys])
+
+class CsdCompat(object):
+
+    def __init__(self, host='localhost', port=6379, *args, **kwargs):
+        redis = Redis(host, port)
+        consolidations = {
+            'last_hour': (60, 60),
+            'last_day': (60 * 60, 24),
+            'last_week': (60 * 60 * 24, 7),
+            'last_month': (60 * 60 * 24, 30),
+            'total': (None, None),
+            }
+        self.edgy = Edgy(redis, consolidations)
+
+    def __getattr__(self, method):
+        def func(*args, **kwargs):
+            return getattr(self.edgy, method)(*args, **kwargs)
+        setattr(self, method, func)
+        return func
+
 if __name__ == '__main__':
-    from time import sleep
-    consolidations = {
-        'last_hour': (60, 60),
-        'last_day': (60 * 60, 24),
-        'total': (None, None),
-        }
-    e = Edgy(Redis(), 1, 60)
-    s = time()
-    for i in range(40000):
-        e.update(s)
-    print '%.3f' % (time() -s) 
-
-    print e.get(s)
-
-    s = time()
-    for i in range(100000):
-        e.get(s)
-    print '%.3f' % (time() -s) 
-
     from csd import Csd
 
-    e = Csd()
+    x = 'x' * 80 + str(time())
 
-    s = time()
-    for i in range(40000):
-        e.update(s, 1)
-    print '%.3f' % (time() -s) 
+    for e in (CsdCompat(), Csd()):
+        s = time()
+        for i in range(40000):
+            e.update(x, 1)
+        print '%.3f' % ((time() -s) / 40.)
 
-    print e.get(s)
+        print e.get(x)
 
-    s = time()
-    for i in range(100000):
-        e.get(s)
-    print '%.3f' % (time() -s) 
-
+        s = time()
+        for i in range(10000):
+            e.get(x)
+        print '%.3f' % ((time() -s) / 10.)
